79e2ecfdea3bbae598bb7acdae39a3b4e24338b7
Jeanne Boyarsky <nyjeanne@gmail.com>
2017-09-03 15:13:52 -0400

    add java 8 support

--- san-francisco/src/main/java/com/javaone/hol2017/junit5/FishermansWharf.java
+++ san-francisco/src/main/java/com/javaone/hol2017/junit5/FishermansWharf.java
@@ -1,9 +1,25 @@
 package com.javaone.hol2017.junit5;
 
+import java.util.*;
+
 public class FishermansWharf {
-	
+
+	private List<SeaLion> seaLions;
+
+	public FishermansWharf() {
+		seaLions = new ArrayList<>();
+	}
+
 	public String getUrl() {
 		return "http://www.fishermanswharf.org";
+	}
+
+	public void addSeaLion(SeaLion s) {
+		seaLions.add(s);
+	}
+
+	public Optional<SeaLion> getOldestSeaLion() {
+		return seaLions.stream().max((s1, s2) -> s1.getAge() - s2.getAge());
 	}
 
 }
--- san-francisco/src/test/java/com/javaone/hol2017/junit5/FishermansWharfTest.java
+++ san-francisco/src/test/java/com/javaone/hol2017/junit5/FishermansWharfTest.java
@@ -17,5 +17,26 @@ public class FishermansWharfTest {
 	public void url() {
 		assertEquals("url", "http://www.fishermanswharf.org", wharf.getUrl());
 	}
+	
+	@Test
+	public void oldestSeaLionForEmptyList() {
+		assertFalse(wharf.getOldestSeaLion().isPresent());
+	}
+	
+	@Test
+	public void oldestSeaLionForFirstElement() {
+		wharf.addSeaLion(new SeaLion(15, 200, true));
+		wharf.addSeaLion(new SeaLion(10, 150, false));
+		SeaLion actual = wharf.getOldestSeaLion().get();
+		assertEquals(15, actual.getAge());
+	}
+	
+	@Test
+	public void oldestSeaLionForLastElement() {
+		wharf.addSeaLion(new SeaLion(10, 150, false));
+		wharf.addSeaLion(new SeaLion(15, 200, true));
+		SeaLion actual = wharf.getOldestSeaLion().get();
+		assertEquals("oldest", 15, actual.getAge());
+	}
 
 }
